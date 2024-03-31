fa13c4968331452d64338ff62587988cb4baa5ac
Felix Oyioka <nyamarifelix2018@gmail.com>
2024-03-31 18:16:38 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#name
+name = input("Enter your name: ")
+#age
+age = input("Enter your age: ")
+#location
+location = input("Enter your location: ")
+# Printing out
+print(f"Hello {name}, you are {age} years old and live in {location}.")
